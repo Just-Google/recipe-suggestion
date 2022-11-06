c103f610edd5b95d8acfcfe01e64dd3d1dbfbed5
muhaiminsarker <sarker.muhaimin15@gmail.com>
2022-11-06 01:52:24 -0500

    basic website set up

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import NavBar from "./Nav";
+import Footer from "./Footer";
+import Header from "./Header";
+import Ingredient from "./Ingredient";
+const Home = () => {
+
+return (
+    <div>
+    <NavBar></NavBar>
+    <Header></Header>
+    <Ingredient></Ingredient>
+    <Footer></Footer>
+    </div>
+)
+}
+
+export default Home;
